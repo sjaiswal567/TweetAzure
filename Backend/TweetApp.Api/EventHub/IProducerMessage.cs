d7069266d4901bfab2934668d38356bdfc53a8ef
unknown <cogdotnet1028@iiht.tech>
2022-09-18 02:13:53 +0530

    first

---
+++ Backend/TweetApp.Api/EventHub/IProducerMessage.cs
@@ -0,0 +1,7 @@
+﻿namespace TweetApp.Api.EventHub
+{
+    public interface IProducerMessage<in TValue> where TValue : class
+    {
+        Task ProduceAsync(TValue value);
+    }
+}
---
+++ Backend/TweetApp.Api/EventHub/ProducerMessage.cs
@@ -0,0 +1,26 @@
+﻿using Azure.Messaging.EventHubs;
+using Azure.Messaging.EventHubs.Producer;
+using System.Text;
+
+namespace TweetApp.Api.EventHub
+{
+    public class ProducerMessage<TValue> : IDisposable, IProducerMessage<TValue> where TValue : class
+    {
+        EventHubProducerClient _producer;
+        public ProducerMessage(IConfigurationSection eventHubConfig)
+        {
+            _producer = new EventHubProducerClient(eventHubConfig["connectionstring"], eventHubConfig["EventHubName"]);
+        }
+        public async void Dispose()
+        {
+            await _producer.DisposeAsync();
+        }
+
+        public async Task ProduceAsync(TValue value)
+        {
+            using EventDataBatch eventBatch = await _producer.CreateBatchAsync();
+            eventBatch.TryAdd(new EventData(Encoding.UTF8.GetBytes($"Event {value}")));
+            await _producer.SendAsync(eventBatch);
+        }
+    }
+}
